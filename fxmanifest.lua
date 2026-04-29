c06a2c23d3eea85738a61b88b68bc41a078aa288
Darka <anthony.perrot.92@gmail.com>
2026-04-29 16:26:01 +0200

    Add FiveM resource manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Top Serveur'
+description 'Plugin de vote FiveM pour Top Serveur'
+version '3.0.0'
+
+server_scripts {
+  'config.lua',
+  'server.lua'
+}
